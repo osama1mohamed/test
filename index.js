29ece4d008ba7eb93f3de30eb45033abb3f8ac49
osama1mohamed <96703014+osama1mohamed@users.noreply.github.com>
2024-07-20 16:51:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-import express from 'express';
-import connectDB from './config/db.js';
-import dotenv from 'dotenv';
+import express from 'express'
+import connectDB from './config/db.js'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-import userRoutes from './routes/user.routes.js';
-import companyRoutes from './routes/company.routes.js';
-import jobRoutes from './routes/job.routes.js';
+import userRoutes from './routes/user.routes.js'
+import companyRoutes from './routes/company.routes.js'
+import jobRoutes from './routes/job.routes.js'
 
-const app = express();
+const app = express()
 
 // Connect to database
-connectDB();
+connectDB()
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
 
 // Routes
-app.use('/users', userRoutes);
-app.use('/company', companyRoutes);
-app.use('/job', jobRoutes);
+app.use('/users', userRoutes)
+app.use('/company', companyRoutes)
+app.use('/job', jobRoutes)
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
