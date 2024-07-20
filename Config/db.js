e4b2faf85c21fde9ab5e74f67150768a365c8dc0
osama1mohamed <96703014+osama1mohamed@users.noreply.github.com>
2024-07-20 17:07:24 +0300

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -1,16 +1,16 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected...');
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('MongoDB connected...')
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1);
+    console.error('Error connecting to MongoDB:', error.message)
+    process.exit(1)
   }
-};
+}
 
-export default connectDB;
+export default connectDB
