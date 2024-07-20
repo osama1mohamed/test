b161bb2e591812c84c8c7831aece57980d09fd21
osama1mohamed <96703014+osama1mohamed@users.noreply.github.com>
2024-07-20 17:07:52 +0300

    Update dotenv.js

--- Config/dotenv.js
+++ Config/dotenv.js
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
 // Environment variables
-const { PORT, MONGODB_URI, JWT_SECRET } = process.env;
+const { PORT, MONGODB_URI, JWT_SECRET } = process.env
 
 export default {
   PORT: PORT || 3000,
   MONGODB_URI,
   JWT_SECRET
-};
+}
